c1cf8b821e54ac4ae3b55966a76e8c0806c02482
Alessandro Lodi <alelod96@gmail.com>
2020-10-16 11:50:11 +0200

    Generator refactoring

--- generator.py
+++ generator.py
@@ -18,64 +18,61 @@ def edge_to_remove(graph):
 
 	return edge
 
-def girvan_newman(graph):
+def girvan_newman(graph, num_of_clusters):
 	# find number of connected components
 	sg = nx.connected_components(graph)
 	sg_count = nx.number_connected_components(graph)
 
+	prev_count = sg_count
 	while(sg_count != num_of_clusters):
 		graph.remove_edge(edge_to_remove(graph)[0], edge_to_remove(graph)[1])
+		print("Edge removed")
+
 		sg = nx.connected_components(graph)
 		sg_count = nx.number_connected_components(graph)
 
+		if(prev_count != sg_count):
+			print("Suddivisi %d cluster" % sg_count)
+			prev_count = sg_count
+
 	return sg
 
-parser = argparse.ArgumentParser(description='Graph generator for clubfs')
+parser = argparse.ArgumentParser(description='Generatore di istanze di CLUBFS')
 
 # Optional params
 parser.add_argument('--seed', action='store', type=int)
 
 # Required params
-parser.add_argument('file_name', metavar='file_name', type=str, help='Name of the file')
+parser.add_argument('file_name', metavar='file_name', type=str, help='Nome del file di output')
 
 # Clustering options
-parser.add_argument('-clu', '--clustering', action='store', nargs=1, type=str, default=['spectral'], choices=['louvain','spectral', 'girvan-newman'], help='Clustering algorithm (default: spectral)')
+parser.add_argument('-clu', '--clustering', action='store', nargs=1, type=str, default=['spectral'], choices=['spectral', 'girvan-newman'], help='Algoritmo di clustering (default: spectral)')
 
 # Graph generator options
-parser.add_argument('-gen', '--generation', action='store', nargs=1, type=str, default=['lfr-benchmark'], choices=['file','lfr-benchmark','partition', 'internet-as'], help='Generation algorithm (default: partition)')
+parser.add_argument('-gen', '--generation', action='store', nargs=1, type=str, default=['lfr-benchmark'], choices=['file','lfr-benchmark','partition', 'internet-as'], help='Algoritmo di generazione (default: partition)')
 
-parser.add_argument('--file-path', action='store', type=str, help='')
+parser.add_argument('--file-path', action='store', type=str, help='Path del dataset')
 
-parser.add_argument('-t1', action='store', type=float, help='Parameter for LFR benchmark')
-parser.add_argument('-t2', action='store', type=float, help='Parameter for LFR benchmark')
-parser.add_argument('-mu', action='store', type=float, help='Parameter for LFR benchmark')
-parser.add_argument('-ad','--avg_degree', action='store', type=float, help='Parameter for LFR benchmark')
-parser.add_argument('-mind','--min_degree', action='store', type=float, help='Parameter for LFR benchmark')
-parser.add_argument('-maxd','--max_degree', action='store', type=float, help='Parameter for LFR benchmark')
-parser.add_argument('-minc','--min_community', action='store', type=float, help='Parameter for LFR benchmark')
-parser.add_argument('-maxc','--max_community', action='store', type=float, help='Parameter for LFR benchmark')
+parser.add_argument('-t1', action='store', type=float, help='t1 per LFR benchmark')
+parser.add_argument('-t2', action='store', type=float, help='t2 Per LFR benchmark')
+parser.add_argument('-mu', action='store', type=float, help='mu Per LFR benchmark')
+parser.add_argument('-ad','--avg_degree', action='store', type=float, help='Degree medio per LFR benchmark, non inserire se specificato --min_degree')
+parser.add_argument('-mind','--min_degree', action='store', type=float, help='Degree minimo per LFR benchmark, non inserire se specificato --avg_degree')
+parser.add_argument('-maxd','--max_degree', action='store', type=float, help='Degree massimo per LFR benchmark')
+parser.add_argument('-minc','--min_community', action='store', type=float, help='Numero minimo di nodi per comunità per LFR benchmark')
+parser.add_argument('-maxc','--max_community', action='store', type=float, help='Numero massimo di nodi per comunità per LFR benchmark')
 
 
-parser.add_argument('-c', '--communities', action='store', nargs='+', type=int, help='Parameter for Partition Graph, list of cardinalities of communities')
-parser.add_argument('-p_in', action='store', type=float, help='Parameter for Partition Graph, probability of edges in the community')
-parser.add_argument('-p_out', action='store', type=float, help='Parameter for Partition Graph, probability of edges between community')
-parser.add_argument('-c_min', action='store', type=float, help='Parameter for Partition Graph, probability of edges in the community')
-parser.add_argument('-c_max', action='store', type=float, help='Parameter for Partition Graph, probability of edges between community')
+parser.add_argument('-c', '--communities', action='store', nargs='+', type=int, help='Lista delle cardinalità delle comunità, scelte a random se non specificato')
+parser.add_argument('-p_in', action='store', type=float, help='Probabilità degli archi all\'interno della comunità')
+parser.add_argument('-p_out', action='store', type=float, help='Probabilità degli archi tra le comunità')
+parser.add_argument('-c_min', action='store', type=float, help='Numero minimo di nodi per comunità, in caso -c non presente')
+parser.add_argument('-c_max', action='store', type=float, help='Numero massimo di nodi per comunità, in caso -c non presente')
 
-parser.add_argument('-n', action='store', type=int, help='Parameter for Internet AS Graph, Number of nodes')
-parser.add_argument('-k', action='store', type=int, help='Number of cluster')
+parser.add_argument('-n', action='store', type=int, help='Numero di nodi')
+parser.add_argument('-k', action='store', type=int, help='Numero di cluster')
 
 args = parser.parse_args()
-
-# Argument check
-# if args.generation[0] == 'lfr-benchmark' and (args.n is None or args.t1 is None or args.t2 is None or args.mu is None or args.min_community is None or (args.avg_degree is None and args.min_degree is None)):
-# 	parser.error("Partition Graph requires --communities, -p_in and -p_out.")
-
-if args.generation[0] == 'partition' and (args.p_in is None or args.p_out is None or args.k is None):
-	parser.error("Partition Graph requires -p_in and -p_out.")
-
-if args.generation[0] == 'internet-as' and args.n is None:
-	parser.error("Internet AS Graph requires -n.")
 
 if args.seed is None: 
 	np.random.seed()
@@ -146,20 +143,32 @@ if args.generation[0] == 'file':
 		f.close()
 
 	for i, link in enumerate(res_body):
-		u = link.split()[0]
-		v = link.split()[1]
-		# u=int(u) -1 
-		# v=int(v) -1
+		u = int(link.split()[0])
+		v = int(link.split()[1])
+		u = u - 1 
+		v = v - 1
 		G.add_edge(u, v)
 
 if not nx.is_connected(G):
+	print("Grafo disconnesso, verrà scelta la componente connessa più grande")
 	largest_cc = max(nx.connected_components(G), key=len)
 	G = G.subgraph(largest_cc).copy()
 
-# spl = nx.average_shortest_path_length(G)
-# cost = (2*len(G.edges)) / (len(G.nodes)*(len(G.nodes)-1))
-# clu = nx.average_clustering(G)
-# print(spl,'&',clu,'&',cost)
+	H = G.__class__()
+	remap_nodes = dict()
+	for i,node in enumerate(G.nodes):
+		remap_nodes[node] = i
+
+	for u,v in sorted(G.edges):
+		if u != v:
+			H.add_edge(int(remap_nodes[u]), int(remap_nodes[v]))
+	
+	G = H
+
+spl = nx.average_shortest_path_length(G)
+cost = (2*len(G.edges)) / (len(G.nodes)*(len(G.nodes)-1))
+clu = nx.average_clustering(G)
+print(spl,'&',clu,'&',cost)
 
 num_of_clusters = args.k if args.generation[0] != 'lfr-benchmark' else cluster_number-1
 
@@ -167,6 +176,19 @@ num_of_clusters = args.k if args.generation[0] != 'lfr-benchmark' else cluster_n
 if args.generation[0] != 'lfr-benchmark':
 	adj_mat = nx.to_numpy_matrix(G)
 		
+	# Louvain
+	if args.clustering[0] == 'louvain':
+		im = algorithms.louvain(G)
+
+		cluster_labels = []
+		nx.set_node_attributes(G, cluster_labels, 'cluster_label')
+		num_of_clusters = 0
+		for (node , comm) in im.to_node_community_map().items():
+			G.nodes[node]['cluster_label'] = comm[0]+1
+			if num_of_clusters < comm[0]+1:
+				num_of_clusters = comm[0]+1
+		num_of_clusters += 1
+
 	# SpectralClustering
 	if args.clustering[0] == 'spectral':
 		sc = SpectralClustering(num_of_clusters, affinity='precomputed', n_init=100, assign_labels='discretize')
@@ -182,7 +204,7 @@ if args.generation[0] != 'lfr-benchmark':
 
 	# Girvan-Newman
 	if args.clustering[0] == 'girvan-newman':
-		c = girvan_newman(G.copy())
+		c = girvan_newman(G.copy(),args.k)
 
 		communities = []
 		for i in c:
@@ -193,17 +215,17 @@ if args.generation[0] != 'lfr-benchmark':
 				G.nodes[node]['cluster_label'] = i+1
 
 # Gephi rappresentation
-nx.write_gexf(G, 'gen_'+args.file_name+'_plot.gexf')
+nx.write_gexf(G, args.file_name+'.gexf')
 
 # Algoritm rappresentation
-file = open('gen_'+ args.file_name+'.in','w')
+file = open(args.file_name+'.in','w')
 
 file.write('%d %d %d\n' % (len(G.nodes), len(G.edges), num_of_clusters))
 
-for u, v in G.edges:
+for u,v in sorted(G.edges):
 	file.write('%d %d\n' % (int(u), int(v)))
 
-for node in G.nodes:
+for node in sorted(G.nodes):
 	file.write('%d %d\n' % (int(node), int(G.nodes[node]['cluster_label'])))
 
 file.close()
